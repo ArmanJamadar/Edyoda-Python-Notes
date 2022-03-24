c130e2830962a969fa86b8ed34ad4de0aff82b50
ArmanJamadar <66637219+ArmanJamadar@users.noreply.github.com>
2022-03-24 19:31:10 +0530

    Add files via upload

---
+++ W04D03/module2.py
@@ -0,0 +1,6 @@
+import module1 as mod
+
+# print(mod.pie)
+# print(mod.area("ciRcle",10))
+from subpackage import module4 as mod4
+print(mod4.extract_name("Arman-Ashish-Durga","-"))
---
+++ W04D03/vscode2.py
@@ -0,0 +1,5 @@
+def addnum(par1,par2):
+    return par1+par2
+
+
+print(addnum(2,2))
