92e85d8339739328a65d323161c8a556089dac3f
ArmanJamadar <66637219+ArmanJamadar@users.noreply.github.com>
2022-03-24 19:27:39 +0530

    Add files via upload

---
+++ W04D01/functions.py
@@ -0,0 +1,13 @@
+def oddeven(var):
+	if var%2==0:
+		return "Even"
+	else:
+		return "Odd"
+
+print(oddeven(2))
+
+
+def stringcheck(string):
+	return "edyoda" in string.lower()
+
+print(stringcheck("Hii i am edyod"))
---
+++ W04D01/lenfunction.py
@@ -0,0 +1,4 @@
+def len(par):
+	return par
+
+print(len([1,2,3,4]))
